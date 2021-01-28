93ffdf191791191c39170a8763cefdd1da9e30fd
Elizabeth1703 <eangelineandriana@gmail.com>
2021-01-29 02:27:28 +0700

    Edit dan Delete

--- routes/web.php
+++ routes/web.php
@@ -21,3 +21,9 @@ Route::get('/pegawai', 'PegawaiController@index');
 Route::get('/pegawai/tambah','PegawaiController@tambah');
 
 Route::post('/pegawai/store','PegawaiController@store');
+
+Route::get('/pegawai/edit/{id}','PegawaiController@edit');
+
+Route::post('/pegawai/update','PegawaiController@update');
+
+Route::get('/pegawai/hapus/{id}','PegawaiController@hapus');
